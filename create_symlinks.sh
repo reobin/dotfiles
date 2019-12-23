c4739288317fc0f700ce5630141a64f55a3313eb
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-12-23 16:12:08 -0500

    Add create symlinks bash script

---
+++ create_symlinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+directories=(nvim yabai karabiner pgcli)
+files=(.zshrc .skhdrc)
+
+for directory in ${directories[@]}; do
+  if test -d ~/.config/$directory; then
+    echo "remove ~/.config/$directory"
+    rm ~/.config/$directory
+  fi
+  echo "create symlink from ~/git/dotfiles/$directory to ~/.config/$directory \n"
+  ln -s ~/git/dotfiles/$directory ~/.config/$directory
+done
+
+for file in ${files[@]}; do
+  if test -f ~/$file; then
+    echo "remove ~/$file"
+    rm ~/$file
+  fi
+  echo "create symlink from ~/git/dotfiles/$file to ~/$file \n"
+  ln -s ~/git/dotfiles/$file ~/$file
+done
+
