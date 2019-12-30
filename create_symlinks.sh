31a7edf97dab37f666ed3b05b45ec13b00f4332a
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-12-30 12:35:16 -0500

    Beautify create_symlinks script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -8,6 +8,8 @@
 #     ███████║   ██║   ██║ ╚═╝ ██║███████╗██║██║ ╚████║██║  ██╗███████║
 #     ╚══════╝   ╚═╝   ╚═╝     ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
 #
+#     Creates symlinks enabling centralized dotfile management
+#
 
 
 directories=(nvim yabai karabiner pgcli)
@@ -15,26 +17,27 @@ files=(.zshrc .skhdrc .gitconfig)
 
 red="\033[0;31m"
 green="\033[0;32m"
+yellow="\033[0;33m"
 nocolor="\033[0m"
 
-
-printf "create symlinks for .config directories... \n"
-for directory in ${directories[@]}; do
-  if test -d ~/.config/$directory; then
-    printf "${red}remove ~/.config/$directory\n"
-    rm -rf ~/.config/$directory
+printf "${yellow}create symlinks for .config directories... \n"
+for directory in "${directories[@]}"; do
+  if test -d "$HOME/.config/$directory"; then
+    printf "${red}remove ~/.config/%s\n" "$directory"
+    rm -rf "$HOME/.config/$directory"
   fi
-  printf "${green}create symlink from ~/dotfiles/$directory to ~/.config/$directory\n"
-  ln -s ~/dotfiles/$directory ~/.config/$directory
+  printf "${green}create symlink from ~/dotfiles/%s to ~/.config/%s\n\n" "$directory" "$directory"
+  ln -s "$HOME/dotfiles/$directory" "$HOME/.config/$directory"
 done
 
-printf "\n${nocolor}create symlinks for dotfiles... \n"
-for file in ${files[@]}; do
-  if test -f ~/$file; then
-    printf "${red}remove ~/$file\n"
-    rm ~/$file
+printf "\n${yellow}create symlinks for dotfiles... \n"
+for file in "${files[@]}"; do
+  if test -f "$HOME/$file"; then
+    printf "${red}remove ~/%s\n" "$file"
+    rm "$HOME/$file"
   fi
-  printf "${green}create symlink from ~/dotfiles/$file to ~/$file\n"
-  ln -s ~/dotfiles/$file ~/$file
+  printf "${green}create symlink from ~/dotfiles/%s to ~/%s\n\n" "$file" "$file"
+  ln -s "$HOME/dotfiles/$file" "$HOME/$file"
 done
 
+printf "\n${yellow}done\n"
