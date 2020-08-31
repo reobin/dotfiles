c7e93c07a74a29e5d0f867f4f060dd6f518598bc
Robin Gagnon <me@reobin.dev>
2020-08-31 09:58:45 -0400

    Rename macOS util settings

--- install-scripts/dock-animation-killer.sh
+++
@@ -1,2 +0,0 @@
-defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
-defaults write com.apple.Dock autohide-delay -float 0.0001; killall Dock
---
+++ macOS-utils/enable_key_repeat.sh
@@ -0,0 +1,2 @@
+# macOS: Disable the accented character prompt when holding 'e', for example
+defaults write -g ApplePressAndHoldEnabled -bool false
