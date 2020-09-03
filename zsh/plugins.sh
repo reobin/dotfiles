164f853b4dd105a4c10b610130d40bb2a512319d
Robin Gagnon <me@reobin.dev>
2020-09-03 15:30:18 -0400

    zsh: Remove antibody plugins config

--- zsh/plugins.sh
+++
@@ -1,4 +0,0 @@
-source /Users/cortexstudio/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/oh-my-zsh.sh
-fpath+=( /Users/cortexstudio/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh )
-source /Users/cortexstudio/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/cortexstudio/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
