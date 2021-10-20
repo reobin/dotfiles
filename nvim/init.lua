991c3b7f01a37fffd4cb1697b0c35d3e394bdfa8
Robin Gagnon <me@reobin.dev>
2021-10-20 14:53:59 -0500

    feat(nvim): Format init.lua file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,65 +1,63 @@
-require('plugins')
-
+require("plugins")
 
 -- Options --
 
 -- Tabs
-vim.o.autoindent = true             -- Copy indent from current line
-vim.o.tabstop = 2                   -- Number of spaces that a <Tab> counts for
-vim.o.shiftwidth = vim.o.tabstop    -- The amount of indent added
-vim.o.expandtab = true              -- Insert spaces with the <Tab> key
+vim.o.autoindent = true -- Copy indent from current line
+vim.o.tabstop = 2 -- Number of spaces that a <Tab> counts for
+vim.o.shiftwidth = vim.o.tabstop -- The amount of indent added
+vim.o.expandtab = true -- Insert spaces with the <Tab> key
 
 -- Other options
-vim.o.relativenumber = true         -- Use relative number on gutter
-vim.o.hidden = true                 -- Do not unload buffers when switching
+vim.o.relativenumber = true -- Use relative number on gutter
+vim.o.hidden = true -- Do not unload buffers when switching
 
 -- Map leader: <Space>
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Color scheme
-vim.cmd('colorscheme moonfly')
-
+vim.cmd("colorscheme moonfly")
 
 -- General key mappings --
 
-local vimp = require('vimp')
+local vimp = require("vimp")
 
 -- J = Keep the cursor in place while joining lines
-vimp.nnoremap('J', 'mzJ`z')
+vimp.nnoremap("J", "mzJ`z")
 
 -- ee = Explore
-vimp.nnoremap('<leader>ee', ':Explore<cr>')
+vimp.nnoremap("<leader>ee", ":Explore<cr>")
 
 -- r = reload vimrc
-vimp.nnoremap('<leader>r',
+vimp.nnoremap(
+  "<leader>r",
   function()
     -- Remove all previously added vimpeccable maps
     vimp.unmap_all()
 
     -- Make sure all open buffers are saved
-    vim.cmd('silent wa')
+    vim.cmd("silent wa")
 
     -- Execute our vimrc lua file again to add back our maps
-    dofile(vim.fn.stdpath('config') ..  '/init.lua')
+    dofile(vim.fn.stdpath("config") .. "/init.lua")
 
-    print('init.lua reloaded')
+    print("init.lua reloaded")
   end
 )
 
-
 -- Telescope --
 
-vimp.nnoremap('<leader>p', ':Telescope find_files<cr>')
-vimp.nnoremap('<leader>s', ':Telescope live_grep<cr>')
-vimp.nnoremap('<leader>b', ':Telescope buffers<cr>')
+vimp.nnoremap("<leader>p", ":Telescope find_files<cr>")
+vimp.nnoremap("<leader>s", ":Telescope live_grep<cr>")
+vimp.nnoremap("<leader>b", ":Telescope buffers<cr>")
 
-local actions = require('telescope.actions')
-local telescope = require('telescope')
+local actions = require("telescope.actions")
+local telescope = require("telescope")
 
 telescope.setup {
   defaults = {
     mappings = {
-      i = { ['<ESC>'] = actions.close }
+      i = {["<ESC>"] = actions.close}
     }
   }
 }
@@ -67,88 +65,94 @@ telescope.setup {
 -- Treesitter --
 
 treesitter_languages = {
-  'go',
-  'javascript',
-  'lua',
-  'tsx',
-  'typescript',
+  "go",
+  "javascript",
+  "lua",
+  "tsx",
+  "typescript"
 }
 
 polyglot_languages = {
-  'elixir',
+  "elixir"
 }
 
-local treesitter = require('nvim-treesitter.configs')
+local treesitter = require("nvim-treesitter.configs")
 
 treesitter.setup {
   ensure_installed = treesitter_languages,
   highlight = {
     enable = true,
-    disable = polyglot_languages,
-  },
+    disable = polyglot_languages
+  }
 }
 
 vim.g.polyglot_disabled = treesitter_languages
 
-vimp.nnoremap('<leader>lr', ':Telescope lsp_references<cr>')
-vimp.nnoremap('<leader>ld', ':Telescope lsp_definitions<cr>')
-
+vimp.nnoremap("<leader>lr", ":Telescope lsp_references<cr>")
+vimp.nnoremap("<leader>ld", ":Telescope lsp_definitions<cr>")
 
 -- LSP --
 
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 
-vimp.nnoremap('<leader>lh', ':lua vim.lsp.buf.hover()<cr>')
-vimp.nnoremap('<leader>le', ':lua vim.lsp.diagnostic.show_line_diagnostics()<cr>')
+vimp.nnoremap("<leader>lh", ":lua vim.lsp.buf.hover()<cr>")
+vimp.nnoremap("<leader>le", ":lua vim.lsp.diagnostic.show_line_diagnostics()<cr>")
 
 -- typescript, javascript
 nvim_lsp.tsserver.setup {}
 
-
 -- Completion
 
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup {
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
+    {name = "nvim_lsp"},
+    {name = "buffer"}
   },
   mapping = {
-    ['<cr>'] = cmp.mapping.confirm({ select = true }),
-  },
+    ["<cr>"] = cmp.mapping.confirm({select = true})
+  }
 }
-
 
 -- Formatting
 
-local formatter = require('formatter')
+local formatter = require("formatter")
 
 formatter.setup {
   filetype = {
+    lua = {
+      function()
+        return {
+          exe = "luafmt",
+          args = {"--indent-count", 2, "--stdin"},
+          stdin = true
+        }
+      end
+    },
     javascript = {
       function()
         return {
-          exe = 'prettier',
-          stdin = true,
+          exe = "prettier",
+          stdin = true
         }
       end
     },
     typescriptreact = {
       function()
         return {
-          exe = 'prettier',
+          exe = "prettier",
           args = {
             "--stdin-filepath",
-            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+            vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
           },
-          stdin = true,
+          stdin = true
         }
       end
-    },
-  },
+    }
+  }
 }
 
-vimp.nnoremap('<leader>f', ':FormatWrite<cr>')
+vimp.nnoremap("<leader>f", ":FormatWrite<cr>")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,41 +1,43 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(
+  function()
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
 
-  -- init.lua config helper
-  use 'svermeulen/vimpeccable'
+    -- init.lua config helper
+    use "svermeulen/vimpeccable"
 
-  -- colorscheme
-  use 'bluz71/vim-moonfly-colors'
+    -- colorscheme
+    use "bluz71/vim-moonfly-colors"
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} },
-  }
-  use 'kyazdani42/nvim-web-devicons'
+    -- Telescope
+    use {
+      "nvim-telescope/telescope.nvim",
+      requires = {{"nvim-lua/plenary.nvim"}}
+    }
+    use "kyazdani42/nvim-web-devicons"
 
-  -- Treesitter
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    branch = '0.5-compat',
-  }
-  use 'sheerun/vim-polyglot'
+    -- Treesitter
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate"
+    }
+    use {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      branch = "0.5-compat"
+    }
+    use "sheerun/vim-polyglot"
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
+    -- LSP
+    use "neovim/nvim-lspconfig"
 
-  -- Completion
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/nvim-cmp'
+    -- Completion
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/nvim-cmp"
 
-  -- Formatting
-  use 'mhartington/formatter.nvim'
-end)
+    -- Formatting
+    use "mhartington/formatter.nvim"
+  end
+)
