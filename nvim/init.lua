9c3cc1407361a22e5b48e67a8fb9c9e1b090a83a
Robin Gagnon <me@reobin.dev>
2021-11-04 19:00:37 -0500

    feat(nvim): Add lualine config

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,6 +48,47 @@ vimp.nnoremap(
   end
 )
 
+local lualine = require("lualine")
+lualine.setup(
+  {
+    options = {
+      theme = "github",
+      component_separators = "|",
+      section_separators = ""
+    },
+    sections = {
+      lualine_a = {{"mode"}},
+      lualine_b = {{"filename", path = 1}, "branch"},
+      lualine_c = {
+        {
+          "diff",
+          symbols = {added = " ", modified = "⦿ ", removed = " "},
+          diff_color = {
+            added = {fg = "green"},
+            modified = {fg = "orange"},
+            removed = {fg = "darkred"}
+          }
+        }
+      },
+      lualine_x = {},
+      lualine_y = {"filetype", "progress"},
+      lualine_z = {
+        {"location"}
+      }
+    },
+    inactive_sections = {
+      lualine_a = {"filename"},
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {"location"}
+    },
+    tabline = {},
+    extensions = {}
+  }
+)
+
 -- l = open last buffer
 vimp.nnoremap("<leader>l", "<C-^>")
 
