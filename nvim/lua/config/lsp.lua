fb437c745d6bc4d93c974de0999689d9a72d27f3
Robin Gagnon <me@reobin.dev>
2022-06-07 12:08:04 -0500

    fix(nvim): Fix nvim-cmp box disappearing

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -32,20 +32,48 @@ lsp.gopls.setup {}
 
 -- #nvim-cmp --
 
-local cmp = require("cmp")
-
+local cmp = require "cmp"
 cmp.setup {
   snippet = {
     expand = function(args)
-      require "luasnip".lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end
   },
+  mapping = cmp.mapping.preset.insert(
+    {
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true
+      },
+      ["<Tab>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      ),
+      ["<S-Tab>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      )
+    }
+  ),
   sources = {
     {name = "nvim_lsp"},
     {name = "luasnip"},
     {name = "buffer"}
-  },
-  mapping = {
-    ["<cr>"] = cmp.mapping.confirm({select = true})
   }
 }
