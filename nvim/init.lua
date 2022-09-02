640a39f4c30eb41d103c6da397565fe55dbc8006
Robin Gagnon <me@reobin.dev>
2022-09-02 10:06:08 -0500

    feat(nvim): Add xml formatter

--- nvim/init.lua
+++ nvim/init.lua
@@ -258,6 +258,15 @@ formatter.setup {
     typescript = {prettier},
     typescriptreact = {prettier},
     vue = {prettier},
+    xml = {
+      function()
+        return {
+          exe = "xmllint",
+          args = {"--format", vim.api.nvim_buf_get_name(0)},
+          stdin = true
+        }
+      end
+    },
     elixir = {
       function()
         return {
