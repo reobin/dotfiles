053d82d3b486b8c4150e1b007735389a4b96d17f
Robin Gagnon <me@reobin.dev>
2023-01-26 14:54:29 -0500

    nvim: Add LSP servers and mappings

--- nvim/init.lua
+++ nvim/init.lua
@@ -197,39 +197,15 @@ require("nvim-treesitter.configs").setup {
 require("nvim-lsp-installer").setup({automatic_installation = true})
 
 local lspconfig = require("lspconfig")
-local on_attach = function(_, bufnr)
-  local opts = {buffer = bufnr}
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set(
-    "n",
-    "<leader>wl",
-    function()
-      vim.inspect(vim.lsp.buf.list_workspace_folders())
-    end,
-    opts
-  )
-  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-  vim.keymap.set("n", "<leader>so", require("telescope.builtin").lsp_document_symbols, opts)
-end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = {"tsserver", "gopls", "elixirls"}
+local servers = {"gopls", "elixirls", "eslint", "tsserver", "volar"}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = on_attach,
     capabilities = capabilities
   }
 end
