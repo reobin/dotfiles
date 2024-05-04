73df97cea1e77cbfb1b86a0ca419425ed9aa9804
Robin Gagnon <me@reobin.dev>
2024-05-04 08:23:40 -0500

    nvim: Show hidden files in neo-tree

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -13,5 +13,5 @@ map("n", "<leader>gb", ":Git blame<cr>")
 map("n", "<leader>e", ":Neotree focus toggle position=float reveal=true<cr>")
 
 map("n", "<leader>f", function()
-  require("conform").format({ async = true, lsp_fallback = true })
+  require("conform").format { async = true, lsp_fallback = true }
 end)
--- nvim/lua/plugins/configs/neo-tree.lua
+++ nvim/lua/plugins/configs/neo-tree.lua
@@ -6,4 +6,15 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  config = function()
+    require("neo-tree").setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    }
+  end,
 }
