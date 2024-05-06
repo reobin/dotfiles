3d87476f850d91b6ac6650f48a4cecb164e4f5af
Robin Gagnon <me@reobin.dev>
2024-05-06 08:26:16 -0500

    nvim: Change LSP key mappings

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -12,15 +12,6 @@ return {
         require("luasnip.loaders.from_vscode").lazy_load()
       end,
     },
-    {
-      "windwp/nvim-autopairs",
-      config = function()
-        require("nvim-autopairs").setup()
-        local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-        local cmp = require "cmp"
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      end,
-    },
   },
   config = function()
     local cmp = require "cmp"
--- nvim/lua/plugins/configs/lsp.lua
+++ nvim/lua/plugins/configs/lsp.lua
@@ -9,8 +9,8 @@ return {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
       end,
     })
 
