868872daa9e42484a3f5bf5cc2a3d6e06b071f03
Robin Gagnon <me@reobin.dev>
2024-05-08 13:03:22 -0500

    nvim: Change colorscheme to kanagawa

--- nvim/lua/plugins/configs/colorscheme.lua
+++ nvim/lua/plugins/configs/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "sainnhe/sonokai",
+  "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "sonokai"
+    vim.cmd.colorscheme "kanagawa"
   end,
 }
