5d0137e19c2d2efcef078b942825ef37275fac2a
Robin Gagnon <me@reobin.dev>
2024-05-08 23:11:12 -0500

    nvim: Add markdown and vimdoc to treesitter languages

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "tsx", "html", "css", "typescript", "javascript" },
+      ensure_installed = { "lua", "tsx", "html", "css", "typescript", "javascript", "markdown", "vimdoc" },
       highlight = { enable = true, use_languagetree = true },
       indent = { enable = true },
     }
