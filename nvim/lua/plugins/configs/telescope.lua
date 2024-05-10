7d4530d7971f03a1352bb42c6dc3740775b4215c
Robin Gagnon <me@reobin.dev>
2024-05-10 15:28:22 -0500

    nvim: Change key mappings for telescope

--- nvim/lua/plugins/configs/telescope.lua
+++ nvim/lua/plugins/configs/telescope.lua
@@ -5,19 +5,17 @@ return {
   config = function()
     require("telescope").setup {
       defaults = {
+        mappings = {
+          i = {
+            ["<ESC>"] = require("telescope.actions").close,
+          },
+        },
         hidden = true,
         file_ignore_patterns = { "^.git" },
       },
     }
-
-    local builtin = require "telescope.builtin"
     vim.keymap.set("n", "<leader>p", ":Telescope find_files find_command=rg,--hidden,--ignore,--files<cr>")
-    vim.keymap.set("n", "<leader>r", function()
-      builtin.resume()
-    end)
-    vim.keymap.set("n", "<leader>s", function()
-      builtin.live_grep { additional_args = { "--hidden" } }
-    end, {})
-    vim.keymap.set("n", "<leader>b", ":Telescope buffers<cr>")
+    vim.keymap.set("n", "<leader>s", ":Telescope live_grep<cr>")
+    vim.keymap.set("n", "<leader>r", ":Telescope resume<cr>")
   end,
 }
