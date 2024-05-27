8bd6908621ea00d82e02037437fc928a50d7157d
Robin Gagnon <me@reobin.dev>
2024-05-27 08:48:44 -0500

    nvim: Add quickfix plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,6 +12,7 @@ local plugins = {
   require "plugins.configs.harpoon",
   require "plugins.configs.autopairs",
   require "plugins.configs.global-note",
+  require "plugins.configs.quickfix",
 }
 
 require("lazy").setup(plugins, require "plugins.configs.lazy")
