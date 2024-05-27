8f18bdf891ef8988f020355a70c86e0b4affdbdb
Robin Gagnon <me@reobin.dev>
2024-05-27 08:48:08 -0500

    nvim: Add elixir to LSP and Mason

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -3,7 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup {
-      ensure_installed = { "lua", "tsx", "html", "css", "typescript", "javascript", "markdown", "vimdoc" },
+      ensure_installed = { "lua", "tsx", "html", "css", "typescript", "javascript", "markdown", "vimdoc", "elixir" },
       highlight = { enable = true, use_languagetree = true },
       indent = { enable = true },
     }
