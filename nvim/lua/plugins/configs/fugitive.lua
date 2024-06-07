45d007877c6fca1bb58b8f375842a3cbf2010ecc
Robin Gagnon <me@reobin.dev>
2024-06-07 17:10:08 -0500

    nvim: Add keymap for fugitive conflicts resolution

--- nvim/lua/plugins/configs/fugitive.lua
+++ nvim/lua/plugins/configs/fugitive.lua
@@ -1,12 +1,12 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>gb", function()
-      vim.cmd.Git("blame")
-    end)
-    vim.keymap.set("n", "<leader>gl", function()
-      vim.cmd.Git("log")
-    end)
+    vim.keymap.set("n", "<leader>gs", ":Git<cr>")
+    vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
+    vim.keymap.set("n", "<leader>gl", ":Git log<cr>")
+
+    vim.keymap.set("n", "<leader>gc", ":Gvdiffsplit!")
+    vim.keymap.set("n", "<leader>gh", ":diffget //2<cr>")
+    vim.keymap.set("n", "<leader>gl", ":diffget //3<cr>")
   end,
 }
