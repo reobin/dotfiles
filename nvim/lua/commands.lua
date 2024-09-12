56ce686de92b96e5531778af20fc39588e860364
Robin Gagnon <me@reobin.dev>
2024-09-12 11:43:47 -0500

    nvim: Add eslint-lsp to Mason installs

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,3 +1,5 @@
 vim.api.nvim_create_user_command("MasonInstallAll", function()
-  vim.cmd("MasonInstall lua-language-server typescript-language-server stylua prettier gopls elixir-ls css-lsp")
+  vim.cmd(
+    "MasonInstall lua-language-server typescript-language-server stylua prettier gopls elixir-ls css-lsp eslint-lsp"
+  )
 end, {})
