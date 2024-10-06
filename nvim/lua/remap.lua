cc9e6f2bc074915befd1dec7a07469f99f487951
Robin Gagnon <me@reobin.dev>
2024-10-06 13:51:57 -0400

    nvim: Remove netrw keymapping

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,7 +1,6 @@
 vim.keymap.set("n", "<leader>l", "<C-^>") -- open last buffer
 vim.keymap.set("n", "<leader>/", ":noh<cr>") -- remove search highlight
 vim.keymap.set("v", "<leader>y", '"+y') -- copy to clipboard
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- netrw
 
 vim.keymap.set("n", "J", "mzJ`z") -- join line and keep cursor position
 vim.keymap.set("n", "<C-d>", "<C-d>zz") -- move half page down and center
