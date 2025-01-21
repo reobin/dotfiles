bc229e3a9468a572df5ece5cf9897cec362c3ed4
Robin Gagnon <me@reobin.dev>
2025-01-21 09:20:13 -0500

    nvim: Migrate cmp to blink.cmp

--- nvim/lua/plugins/configs/cmp.lua
+++ nvim/lua/plugins/configs/cmp.lua
@@ -1,23 +1,14 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-nvim-lsp",
+  "saghen/blink.cmp",
+  dependencies = "rafamadriz/friendly-snippets",
+  version = "*",
+  opts = {
+    keymap = { preset = "enter" },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+      cmdline = {},
+    },
+    completion = { documentation = { auto_show = true } },
   },
-  config = function()
-    local cmp = require("cmp")
-
-    cmp.setup({
-      mapping = cmp.mapping.preset.insert({
-        ["<cr>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-      }),
-    })
-  end,
+  opts_extend = { "sources.default" },
 }
