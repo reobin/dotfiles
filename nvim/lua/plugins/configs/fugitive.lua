5c45fff481cee5cd141f9bbf027c29d3519ab1b1
Robin Gagnon <me@reobin.dev>
2025-02-08 16:38:45 -0500

    nvim: Open vim fugitive in alternate full screen buffer

--- nvim/lua/plugins/configs/fugitive.lua
+++ nvim/lua/plugins/configs/fugitive.lua
@@ -1,9 +1,8 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<leader>gs", ":Git<cr>")
+    vim.keymap.set("n", "<leader>gs", ":enew | Git | only<cr>")
     vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
-    vim.keymap.set("n", "<leader>gl", ":Git log<cr>")
 
     vim.keymap.set("n", "<leader>gc", ":Gvdiffsplit!")
     vim.keymap.set("n", "<leader>gh", ":diffget //2<cr>")
