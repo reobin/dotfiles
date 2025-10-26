4443913888556d4e52f933136920db640a19776a
Robin Gagnon <me@reobin.dev>
2025-10-25 21:49:32 -0500

    nvim: Change mappings for git diff

--- nvim/lua/plugins/configs/fugitive.lua
+++ nvim/lua/plugins/configs/fugitive.lua
@@ -4,8 +4,8 @@ return {
     vim.keymap.set("n", "<leader>gs", ":enew | Git | only<cr>")
     vim.keymap.set("n", "<leader>gb", ":Git blame<cr>")
 
-    vim.keymap.set("n", "<leader>gc", ":Gvdiffsplit!<cr>")
-    vim.keymap.set("n", "<leader>gh", ":diffget //2<cr>")
-    vim.keymap.set("n", "<leader>gl", ":diffget //3<cr>")
+    vim.keymap.set("n", "dv", ":Gvdiffsplit!<cr>")
+    vim.keymap.set("n", "dh", ":diffget //2<cr>")
+    vim.keymap.set("n", "dl", ":diffget //3<cr>")
   end,
 }
