a2a20017713a72b44dc55d5a5d0e8b7836a3ee4b
Robin Gagnon <me@reobin.dev>
2026-01-23 15:25:26 -0500

    nvim: Add center utils to base keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,7 @@
-vim.keymap.set("n", "<leader>l", "<C-^>")    -- open last buffer
+vim.keymap.set("n", "<leader>l", "<C-^>") -- open last buffer
 vim.keymap.set("n", "<leader>/", ":noh<cr>") -- clear search highlight
+vim.keymap.set("n", "J", "mzJ`z") -- join lines without moving cursor
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- center screen after half-page down
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center screen after half-page up
+vim.keymap.set("n", "n", "nzz") -- center screen after search next
+vim.keymap.set("n", "N", "Nzz") -- center screen after search previous
