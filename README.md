016d2067a95c8870d96d0806327dccc84c928b47
Robin Gagnon <me@reobin.dev>
2026-01-22 20:54:58 -0500

    Remove all dotfiles (clean slate)

--- README.md
+++
@@ -1,7 +0,0 @@
-# `reobin/dotfiles`
-
-## Get started
-
-1. Set up [homebrew](./docs/homebrew.md)
-2. Run some [macOS setup scripts](./bin/README.md)
-3. Set up git [GPG signing](./docs/gpg.md)
--- nvim/lua/plugins/configs/gitsigns.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup()
-    vim.keymap.set("n", "<leader>gj", ":Gitsigns next_hunk<cr>")
-    vim.keymap.set("n", "<leader>gk", ":Gitsigns prev_hunk<cr>")
-  end,
-}
