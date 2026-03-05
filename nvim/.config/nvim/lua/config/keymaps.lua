f8eff1da4bd7abef274b87a69ac5ebf25cab17fe
Robin Gagnon <me@reobin.dev>
2026-03-04 21:56:43 -0500

    nvim: Remove move line in normal mode keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,5 +4,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center screen after half-page up
 vim.keymap.set("n", "n", "nzz") -- center screen after search next
 vim.keymap.set("n", "N", "Nzz") -- center screen after search previous
 
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
 vim.keymap.del("i", "<A-j>")
 vim.keymap.del("i", "<A-k>")
