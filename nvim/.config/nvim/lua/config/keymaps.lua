ee3766242235f6719a4d4982524c3969ebc1d42a
Robin Gagnon <me@reobin.dev>
2026-03-04 21:42:46 -0500

    nvim: Remove move line in insert mode keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz") -- center screen after half-page down
 vim.keymap.set("n", "<C-u>", "<C-u>zz") -- center screen after half-page up
 vim.keymap.set("n", "n", "nzz") -- center screen after search next
 vim.keymap.set("n", "N", "Nzz") -- center screen after search previous
+
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
