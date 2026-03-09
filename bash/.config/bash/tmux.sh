5f48e7562095b6cbc22eca66b9dfb98ed2a52f58
Robin Gagnon <me@reobin.dev>
2026-03-09 15:08:30 -0500

    bash: Remove rename window on tmux layout script

--- bash/.config/bash/tmux.sh
+++ bash/.config/bash/tmux.sh
@@ -8,8 +8,6 @@ tl() {
 
   editor_pane="$TMUX_PANE"
 
-  tmux rename-window -t "$editor_pane" "tl"
-
   # split editor pane horizontally - AI on right 40%
   ai_pane=$(tmux split-window -h -p 40 -d -t "$editor_pane" -c "$current_dir" -P -F '#{pane_id}')
 
