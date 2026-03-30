e559427aaea97f9d566df172e8f1840b6cb2daa2
Robin Gagnon <me@reobin.dev>
2026-03-30 09:04:56 -0500

    zsh: Add tab completion on hidden files

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,11 +1,13 @@
 autoload -Uz compinit
 
+setopt GLOB_DOTS
+
 local zcompdump_dir=${XDG_CACHE_HOME:-$HOME/.cache}/zsh
 [[ -d $zcompdump_dir ]] || mkdir -p "$zcompdump_dir"
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 zstyle ':completion:*' squeeze-slashes true
-zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' special-dirs false
 
 compinit -d "$zcompdump_dir/zcompdump"
