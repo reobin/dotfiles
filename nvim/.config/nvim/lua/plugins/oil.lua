5cc276a9ecbd5effdf932866711cf09074cc5463
Robin Gagnon <me@reobin.dev>
2026-04-04 19:27:55 -0500

    nvim: Increase oil float size

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,8 +5,8 @@ return {
     default_file_explore = true,
     skip_confirm_for_simple_edits = true,
     float = {
-      max_width = 0.6,
-      max_height = 0.6,
+      max_width = 0.9,
+      max_height = 0.9,
       border = "rounded",
     },
     keymaps = {
