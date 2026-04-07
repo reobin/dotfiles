ac047a9ba76b75bc6cfed86627d4e5611758f613
Robin Gagnon <me@reobin.dev>
2026-04-07 18:43:56 -0500

    zsh: change history search to beginning search

--- zsh/.config/zsh/history.zsh
+++ zsh/.config/zsh/history.zsh
@@ -10,20 +10,22 @@ setopt HIST_VERIFY
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 
-autoload -Uz up-line-or-search down-line-or-search edit-command-line
+autoload -Uz up-line-or-beginning-search down-line-or-beginning-search edit-command-line
 
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
 zle -N edit-command-line
 
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-bindkey '^[OA' up-line-or-search
-bindkey '^[OB' down-line-or-search
+bindkey '^[[A' up-line-or-beginning-search
+bindkey '^[[B' down-line-or-beginning-search
+bindkey '^[OA' up-line-or-beginning-search
+bindkey '^[OB' down-line-or-beginning-search
 bindkey '^X' edit-command-line
 
 if (( ${+terminfo[kcuu1]} )); then
-  bindkey "$terminfo[kcuu1]" up-line-or-search
+  bindkey "$terminfo[kcuu1]" up-line-or-beginning-search
 fi
 
 if (( ${+terminfo[kcud1]} )); then
-  bindkey "$terminfo[kcud1]" down-line-or-search
+  bindkey "$terminfo[kcud1]" down-line-or-beginning-search
 fi
