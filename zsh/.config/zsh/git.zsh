137e82fc4353f6e4b1bd83f894e807a733de6acd
Robin Gagnon <me@reobin.dev>
2026-05-07 08:01:24 -0500

    git: add gc clone helper

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -1,3 +1,19 @@
+gc() {
+  local repo_url="$1"
+  local target_dir="$2"
+
+  if [[ -z "$repo_url" ]]; then
+    print 'Usage: gc <repository-url> [directory]'
+    return 1
+  fi
+
+  if [[ -n "$target_dir" ]]; then
+    git clone "$repo_url" "$target_dir" && cd "$target_dir"
+  else
+    git clone "$repo_url" && cd "$(basename "$repo_url" .git)"
+  fi
+}
+
 gd() {
   local main current_dir branch reply output
 
