3070f7029f4265e234cbe7bbb6a2531f630d801d
Robin Gagnon <me@reobin.dev>
2026-05-25 08:28:35 -0500

    nvim: replace lazygit with vim fugitive

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,7 @@
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 3
+vim.opt.signcolumn = "yes"
+vim.opt.statuscolumn = ""
 vim.g.lazyvim_prettier_needs_config = false
 vim.opt.conceallevel = 0
 vim.opt.ignorecase = false
