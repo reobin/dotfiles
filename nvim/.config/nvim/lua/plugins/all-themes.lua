a325813677ac53bf0663c5896478755f02dda9ba
Robin Gagnon <me@reobin.dev>
2026-05-26 19:15:51 -0500

    nvim: add zenbones to themes

--- nvim/.config/nvim/lua/plugins/all-themes.lua
+++ nvim/.config/nvim/lua/plugins/all-themes.lua
@@ -108,4 +108,10 @@ return {
     lazy = true,
     priority = 1000,
   },
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = true,
+    priority = 1000,
+  },
 }
