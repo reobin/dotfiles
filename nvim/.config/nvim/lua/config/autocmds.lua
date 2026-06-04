24e42e5ee40927b090eaa97d7db6b508aa0b3039
Robin Gagnon <me@reobin.dev>
2026-06-04 13:22:23 -0500

    nvim: skip cursor restore in rebases

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,1 +1,19 @@
 vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_del_augroup_by_name("lazyvim_last_loc")
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("lazyvim_last_loc", { clear = true }),
+  callback = function(event)
+    local exclude = { "gitcommit", "gitrebase" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
